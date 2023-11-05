356e34b91f79bcf8262e3134e99c8f760d4340b8
Aspera18 <139146594+Aspera18@users.noreply.github.com>
2023-11-05 23:16:09 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,271 @@
+document.getElementById('plus').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number').innerText);
+    if (currentNumber < 5) {
+      currentNumber += 1;
+      document.getElementById('number').innerText = currentNumber;
+    }
+  });
+  
+  document.getElementById('minus').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number').innerText);
+    if (currentNumber > 0) {
+      currentNumber -= 1;
+      document.getElementById('number').innerText = currentNumber;
+    }
+  });
+  
+  
+  document.getElementById('plus2').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number2').innerText);
+    if (currentNumber < 5) {
+      currentNumber += 1;
+      document.getElementById('number2').innerText = currentNumber;
+    }
+  });
+  
+  document.getElementById('minus2').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number2').innerText);
+    if (currentNumber > 0) {
+      currentNumber -= 1;
+      document.getElementById('number2').innerText = currentNumber;
+    }
+  });
+  
+  document.getElementById('plus3').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number3').innerText);
+    if (currentNumber < 5) {
+      currentNumber += 1;
+      document.getElementById('number3').innerText = currentNumber;
+    }
+  });
+  
+  document.getElementById('minus3').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number3').innerText);
+    if (currentNumber > 0) {
+      currentNumber -= 1;
+      document.getElementById('number3').innerText = currentNumber;
+    }
+  });
+
+  document.getElementById('plus4').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number4').innerText);
+    if (currentNumber < 5) {
+      currentNumber += 1;
+      document.getElementById('number4').innerText = currentNumber;
+    }
+  });
+  
+  document.getElementById('minus4').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number4').innerText);
+    if (currentNumber > 0) {
+      currentNumber -= 1;
+      document.getElementById('number4').innerText = currentNumber;
+    }
+  });
+  
+  document.getElementById('plus5').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number5').innerText);
+    if (currentNumber < 5) {
+      currentNumber += 1;
+      document.getElementById('number5').innerText = currentNumber;
+    }
+  });
+  
+  document.getElementById('minus5').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number5').innerText);
+    if (currentNumber > 0) {
+      currentNumber -= 1;
+      document.getElementById('number5').innerText = currentNumber;
+    }
+  });
+
+  document.getElementById('plus6').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number6').innerText);
+    if (currentNumber < 5) {
+      currentNumber += 1;
+      document.getElementById('number6').innerText = currentNumber;
+    }
+  });
+  
+  document.getElementById('minus6').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number6').innerText);
+    if (currentNumber > 0) {
+      currentNumber -= 1;
+      document.getElementById('number6').innerText = currentNumber;
+    }
+  });
+
+  document.getElementById('plus7').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number7').innerText);
+    if (currentNumber < 5) {
+      currentNumber += 1;
+      document.getElementById('number7').innerText = currentNumber;
+    }
+  });
+  
+  document.getElementById('minus7').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number7').innerText);
+    if (currentNumber > 0) {
+      currentNumber -= 1;
+      document.getElementById('number7').innerText = currentNumber;
+    }
+  });
+
+  document.getElementById('plus8').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number8').innerText);
+    if (currentNumber < 5) {
+      currentNumber += 1;
+      document.getElementById('number8').innerText = currentNumber;
+    }
+  });
+  
+  document.getElementById('minus8').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number8').innerText);
+    if (currentNumber > 0) {
+      currentNumber -= 1;
+      document.getElementById('number8').innerText = currentNumber;
+    }
+  });
+
+  document.getElementById('plus9').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number9').innerText);
+    if (currentNumber < 5) {
+      currentNumber += 1;
+      document.getElementById('number9').innerText = currentNumber;
+    }
+  });
+  
+  document.getElementById('minus9').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number9').innerText);
+    if (currentNumber > 0) {
+      currentNumber -= 1;
+      document.getElementById('number9').innerText = currentNumber;
+    }
+  });
+  document.getElementById('plus10').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number10').innerText);
+    if (currentNumber < 5) {
+      currentNumber += 1;
+      document.getElementById('number10').innerText = currentNumber;
+    }
+  });
+  
+  document.getElementById('minus10').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number10').innerText);
+    if (currentNumber > 0) {
+      currentNumber -= 1;
+      document.getElementById('number10').innerText = currentNumber;
+    }
+  });
+
+  document.getElementById('plus11').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number11').innerText);
+    if (currentNumber < 5) {
+      currentNumber += 1;
+      document.getElementById('number11').innerText = currentNumber;
+    }
+  });
+  
+  document.getElementById('minus11').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number11').innerText);
+    if (currentNumber > 0) {
+      currentNumber -= 1;
+      document.getElementById('number11').innerText = currentNumber;
+    }
+  });
+
+  document.getElementById('plus12').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number12').innerText);
+    if (currentNumber < 5) {
+      currentNumber += 1;
+      document.getElementById('number12').innerText = currentNumber;
+    }
+  });
+  
+  document.getElementById('minus12').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number12').innerText);
+    if (currentNumber > 0) {
+      currentNumber -= 1;
+      document.getElementById('number12').innerText = currentNumber;
+    }
+  });
+
+  document.getElementById('plus13').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number13').innerText);
+    if (currentNumber < 5) {
+      currentNumber += 1;
+      document.getElementById('number13').innerText = currentNumber;
+    }
+  });
+  
+  document.getElementById('minus13').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number13').innerText);
+    if (currentNumber > 0) {
+      currentNumber -= 1;
+      document.getElementById('number13').innerText = currentNumber;
+    }
+  });
+
+  document.getElementById('plus14').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number14').innerText);
+    if (currentNumber < 5) {
+      currentNumber += 1;
+      document.getElementById('number14').innerText = currentNumber;
+    }
+  });
+  
+  document.getElementById('minus14').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number14').innerText);
+    if (currentNumber > 0) {
+      currentNumber -= 1;
+      document.getElementById('number14').innerText = currentNumber;
+    }
+  });
+
+  document.getElementById('plus15').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number15').innerText);
+    if (currentNumber < 5) {
+      currentNumber += 1;
+      document.getElementById('number15').innerText = currentNumber;
+    }
+  });
+  
+  document.getElementById('minus15').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number15').innerText);
+    if (currentNumber > 0) {
+      currentNumber -= 1;
+      document.getElementById('number15').innerText = currentNumber;
+    }
+  });
+
+  document.getElementById('plus16').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number16').innerText);
+    if (currentNumber < 5) {
+      currentNumber += 1;
+      document.getElementById('number16').innerText = currentNumber;
+    }
+  });
+  
+  document.getElementById('minus16').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number16').innerText);
+    if (currentNumber > 0) {
+      currentNumber -= 1;
+      document.getElementById('number16').innerText = currentNumber;
+    }
+  });
+
+  document.getElementById('plus17').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number17').innerText);
+    if (currentNumber < 5) {
+      currentNumber += 1;
+      document.getElementById('number17').innerText = currentNumber;
+    }
+  });
+  
+  document.getElementById('minus17').addEventListener('click', function() {
+    let currentNumber = parseInt(document.getElementById('number17').innerText);
+    if (currentNumber > 0) {
+      currentNumber -= 1;
+      document.getElementById('number17').innerText = currentNumber;
+    }
+  });
